7779faed07ddeea3237944c2a4078cf4163c9afd
Tod D. Romo <drtromo@gmail.com>
2022-11-14 14:36:11 -0500

    Removing verbose flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 set -xe
 
 cmake CMakeLists.txt ${CMAKE_ARGS} -DPython3_EXECUTABLE="$PYTHON" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release 
-cmake --build . -j ${CPU_COUNT} --verbose
-cmake --install . --verbose
+cmake --build . -j ${CPU_COUNT}
+cmake --install .
